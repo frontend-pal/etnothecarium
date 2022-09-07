e5742de2fa7933d3f9fd04fa7539cd2e4f5a6fb3
Joel Alvarez <bjalvarez@alianza.com.co>
2022-09-06 20:15:46 -0500

    ajuste Idioma ingles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'index', loadChildren: () => import('./components/pages/index/index.module').then(m => m.IndexModule) },
+  { path: 'home', loadChildren: () => import('./components/pages/index/index.module').then(m => m.IndexModule) },
   { path: 'products', loadChildren: () => import('./components/pages/products/products.module').then(m => m.ProductsModule) },
   { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
   { path: 'research', loadChildren: () => import('./components/pages/research/research.module').then(m => m.ResearchModule) },
@@ -10,12 +10,12 @@ const routes: Routes = [
   { path: 'store', loadChildren: () => import('./components/pages/contact/contact.module').then(m => m.ContactModule) },
   {
     path: '',
-    redirectTo: '/index',
+    redirectTo: '/home',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: '/index',
+    redirectTo: '/home',
     pathMatch: 'full'
   }
 ];
